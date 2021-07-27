39e6427d87e5282debcccf56cfb6b91494dba6b0
Kezuki <constantin.bibikov@yandex.ru>
2021-07-27 18:30:42 +0300

    save

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -11,21 +11,23 @@ const InputField = ({
     onBlur,
     error,
 }) => {
-    const labelExtraClass = value ? "input-field__label_active" : "";
-    const fieldExtraClass = error ? "input-field__label_error" : "";
+    const labelExtraClass = value ? "input-field__label-text_active" : "";
+    const fieldExtraClass = error ? "input-field__label-text_error" : "";
 
     return (
-        <div className={`input-field ${fieldExtraClass} ${className}`}>
-            <input
-                className="input-field__item"
-                type={type}
-                name={name}
-                value={value}
-                onChange={(e) => onChange(e)}
-                onBlur={(e) => onBlur(e)}
-            />
-            <label className={`input-field__label ${labelExtraClass}`}>
-                {label}
+        <div className={`${className} input-field ${fieldExtraClass}`}>
+            <label className={`input-field__label `}>
+                <input
+                    className="input-field__item"
+                    type={type}
+                    name={name}
+                    value={value}
+                    onChange={(e) => onChange(e)}
+                    onBlur={(e) => onBlur(e)}
+                />
+                <div className={`input-field__label-text ${labelExtraClass}`}>
+                    {label}
+                </div>
             </label>
             <div className="input-field__error">{error}</div>
         </div>
